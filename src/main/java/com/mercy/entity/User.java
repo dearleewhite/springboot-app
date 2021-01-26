a2e2091f595223d6d50d3b009e4e4bdc42cec12d
huwl <18672309550@163.com>
2021-01-26 18:19:03 +0800

    add: 引入redis缓存数据 add: 两个集合根据多个属性取差集(通用类以及属性)

--- src/main/java/com/mercy/entity/User.java
+++ src/main/java/com/mercy/entity/User.java
@@ -1,14 +1,11 @@
 package com.mercy.entity;
 
+import com.google.common.collect.Lists;
+import com.mercy.utils.CollectionUtil;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.experimental.Accessors;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -27,14 +24,29 @@ public class User extends BaseEntity {
     private String name;
 
 
-    public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        List<Integer> list1 = Arrays.asList(8,3, 4, 5);
-        System.out.println(list);
+    public static void main(String[] args) throws Exception {
+        List<Type> list1 = Lists.newArrayList();
+        for (int i = 0; i < 12; i++) {
+            Type type = new Type();
+            type.setName("name" + i);
+            type.setPid(1L);
+            type.setLevel(i);
+            list1.add(type);
+        }
+        List<Type> list2 = Lists.newArrayList();
+        for (int i = 0; i < 10; i++) {
+            Type type = new Type();
+            type.setName("name" + i);
+            type.setPid(1L);
+            list2.add(type);
+        }
+        try {
+            List<Type> objects = (List<Type>)CollectionUtil.receiveDefectListByClassAndFieldNames(list1, list2, Type.class, "name", "pid","亚索看好了");
+            System.out.println(objects);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
 
     }
 }
--- src/main/java/com/mercy/utils/CollectionUtil.java
+++ src/main/java/com/mercy/utils/CollectionUtil.java
@@ -1,10 +1,16 @@
 package com.mercy.utils;
 
 import com.mercy.entity.Type;
-import com.mercy.entity.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.util.CollectionUtils;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @Description: 集合工具类
@@ -12,7 +18,7 @@ import java.util.stream.Collectors;
  * @Date: 2021-01-26 14:31
  **/
 
-
+@Slf4j
 public class CollectionUtil {
 
     /**
@@ -28,5 +34,76 @@ public class CollectionUtil {
                         .collect(Collectors.toList()).contains(item.getName() + "|" + item.getPid()))
                 .collect(Collectors.toList());
         return resultList;
+    }
+
+    /**
+     * @param firstArrayList  第一个ArrayList
+     * @param secondArrayList 第二个ArrayList
+     * @return resultList 差集ArrayList
+     * @方法描述：获取两个ArrayList的差集
+     */
+    public static List<?> receiveDefectListByClassAndFieldNames(List<?> firstArrayList, List<?> secondArrayList, Class<?> clazz, String... fieldNames) throws Exception {
+        if (CollectionUtils.isEmpty(firstArrayList)) {
+            return Collections.emptyList();
+        }
+        if (!clazz.isInstance(firstArrayList.get(0))) {
+            throw new Exception("集合的元素类与指定类的类型不一致!");
+        }
+        // 类型一致做转换
+        List<Object> objectList1 = translate(firstArrayList, clazz);
+        if (CollectionUtils.isEmpty(secondArrayList)) {
+            return objectList1;
+        }
+        if (!clazz.isInstance(secondArrayList.get(0))) {
+            throw new Exception("集合的元素类与指定类的类型不一致!");
+        }
+        // 类型一致做转换
+        List<Object> objectList2 = translate(secondArrayList, clazz);
+        List<String> fieldNamesFromPara = Arrays.stream(fieldNames).collect(Collectors.toList());
+        if (fieldNamesFromPara.size() < 1) {
+            throw new Exception();
+        }
+        Object o = objectList1.get(0);
+        Field[] fields = o.getClass().getDeclaredFields();
+        List<String> fieldNameList = Stream.of(fields).map(Field::getName).collect(Collectors.toList());
+        List<String> fieldNamesError = fieldNamesFromPara.stream().filter(fieldNameFromPara -> !fieldNameList.contains(fieldNameFromPara)).collect(Collectors.toList());
+        if (!CollectionUtils.isEmpty(fieldNamesError)) {
+            fieldNamesFromPara.removeAll(fieldNamesError);
+            System.out.println("参数传入的以下属性不存在:" + fieldNamesError.toString());
+        }
+        return objectList1.stream()
+                .filter(item -> !objectList2.stream()
+                        .map(e -> getString(e, fieldNamesFromPara)).collect(Collectors.toList())
+                        .contains(getString(item, fieldNamesFromPara)))
+                .collect(Collectors.toList());
+    }
+
+    private static List<Object> translate(List<?> secondArrayList, Class<?> clazz) {
+        return secondArrayList.stream().map(t -> {
+            Object obj = null;
+            try {
+                obj = clazz.newInstance();
+            } catch (InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+            BeanUtils.copyProperties(t, obj);
+            return obj;
+        }).collect(Collectors.toList());
+    }
+
+
+    private static String getString(Object e, List<String> fieldNames) {
+        StringBuilder sb = new StringBuilder();
+        for (String fieldName : fieldNames) {
+            try {
+                Field field = e.getClass().getDeclaredField(fieldName);
+                field.setAccessible(true);
+                sb.append(field.get(e)).append("|");
+            } catch (NoSuchFieldException | IllegalAccessException noSuchFieldException) {
+                noSuchFieldException.printStackTrace();
+            }
+
+        }
+        return sb.toString();
     }
 }
